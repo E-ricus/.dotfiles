77128ac87485f48eb1249df6cd4331e18b4996ff
Eric Puentes <eric.david2333@gmail.com>
2021-03-09 18:09:49 -0500

    small tweaks

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,6 +3,7 @@ abbr -a c cargo
 abbr -a vimdiff 'nvim -d'
 
 set PATH $PATH /usr/local/go/bin
+set PATH $PATH $HOME/go/bin
 set GOPATH $HOME/go
 set GOROOT /usr/local/go
 set PATH $HOME/.cargo/bin $PATH
@@ -12,6 +13,8 @@ set PATH $HOME/.local/bin $PATH
 setenv FZF_DEFAULT_COMMAND 'rg --files'
 setenv FZF_CTRL_T_COMMAND 'rg --files'
 setenv FZF_DEFAULT_OPTS '--height 20%'
+
+#setenv BAT_THEME gruvbox
 
 starship init fish | source
 
