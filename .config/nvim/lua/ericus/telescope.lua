cbf12e0693791a2cb4bdfc2434f6ed7b5c7c0ff5
Eric Puentes <eric.david2333@gmail.com>
2021-05-15 20:16:36 -0500

    telescope themes

--- .config/nvim/lua/ericus/telescope.lua
+++ .config/nvim/lua/ericus/telescope.lua
@@ -1,4 +1,5 @@
 local actions = require('telescope.actions')
+
 require('telescope').setup {
     defaults = {
         prompt_prefix = ' 🔍',
@@ -24,7 +25,8 @@ require('telescope').setup {
 require('telescope').load_extension('fzf')
 
 -- Keymaps
-local map = require('ericus.vim-utils').mapper
+local utils = require('ericus.vim-utils')
+local map = utils.mapper
 
 map('n', '<leader>ff', 'Telescope find_files')
 map('n', '<leader>f.', 'Telescope find_files find_command=rg,--ignore,--hidden,--files')
@@ -34,3 +36,22 @@ map('n', '<leader>fb', 'Telescope buffers')
 map('n', '<leader>fh', 'Telescope help_tags')
 map('n', '<leader>ts', 'Telescope treesitter')
 map('n', '<leader>fq', 'Telescope quickfix')
+utils.lua_mapper('n', '<leader>th', "require('ericus.telescope').theme()")
+
+local M = {}
+
+-- Colorscheme but with transparent background
+M.theme = function ()
+    require('telescope.builtin').colorscheme({
+        attach_mappings = function(_)
+            actions.select_default:enhance({
+                post = function ()
+                    vim.cmd('hi Normal guibg=NONE ctermbg=NONE')
+                end,
+            })
+            return true
+        end
+    })
+end
+
+return M
