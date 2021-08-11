a7292c90501d8ec490e16f0c5b59e3454e54e327
Eric Puentes <eric.david2333@gmail.com>
2021-08-10 20:50:23 -0500

    improve go tests

--- .config/nvim/lua/ericus/lang-tools.lua
+++ .config/nvim/lua/ericus/lang-tools.lua
@@ -1,20 +1,55 @@
 -- Golang
 local gl = require('gl.test')
+local glm = require('gl.go_mod')
 
 local M = {}
 
 function M.go_test_all()
-    gl.TestRun:new('./...'):run()
-    -- vim.cmd('!go test ./... -v')
+    gl.TestRun:new():run()
 end
 
 
-function M.test_package()
+function M.go_test_package()
     local package = vim.fn.input('Package name: ')
-    gl.TestRun:new(package):run()
+    local module = glm.get().Path
+    local opts = {
+        file_pattern = module .. '/' .. package
+    }
+    gl.TestRun:new(opts):run()
 end
 
+function M.go_test_file()
+    local file_pattern = vim.fn.input('File pattern: ')
+    local opts = {
+        file_pattern = file_pattern
+    }
+    gl.TestRun:new(opts):run()
+end
+
+function M.go_test_pattern()
+    local pattern = vim.fn.input('Test pattern: ')
+    local opts = {
+        test_pattern = pattern
+    }
+    gl.TestRun:new(opts):run()
+end
+
+function M.go_test()
+    local package = vim.fn.input('Package name (empty all): ')
+
+    local pattern = vim.fn.input('Test pattern: ')
+    local module = glm.get().Path
+    local opts = {
+        file_pattern = module .. '/' .. package,
+        test_pattern = pattern
+    }
+    gl.TestRun:new(opts):run()
+end
+
+vim.cmd("command! GoTest lua require('ericus.lang-tools').go_test()")
 vim.cmd("command! GoTestAll lua require('ericus.lang-tools').go_test_all()")
-vim.cmd("command! GoTestPackage lua require('ericus.lang-tools').test_package()")
+vim.cmd("command! GoTestPackage lua require('ericus.lang-tools').go_test_package()")
+vim.cmd("command! GoTestFile lua require('ericus.lang-tools').go_test_file()")
+vim.cmd("command! GoTestPattern lua require('ericus.lang-tools').go_test_pattern()")
 
 return M
