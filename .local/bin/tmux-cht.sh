e5b5f5f949cb49eb76878d7407e0038319e8f2e2
Eric Puentes <eric.david2333@gmail.com>
2021-09-12 17:58:23 -0500

    refactor tmux

--- .local/bin/tmux-cht.sh
+++ .local/bin/tmux-cht.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-selected=`cat ~/.config/tmux/cht-languages ~/.config/tmux/cht-command | fzf`
+selected=`cat $XDG_CONFIG_HOME/tmux/cht-sh/cht-languages $XDG_CONFIG_HOME/tmux/cht-sh/cht-command | fzf`
 
 read -p "Enter Query: " query
 
-if grep -qs "$selected" ~/.config/tmux/cht-languages; then
+if grep -qs "$selected" $XDG_CONFIG_HOME/tmux/cht-sh/cht-languages; then
     query=`echo $query | tr ' ' '+'`
     tmux neww bash -c "curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
 else
