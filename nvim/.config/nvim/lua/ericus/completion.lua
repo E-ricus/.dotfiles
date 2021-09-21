2efbc9488bcc4f83cff2a8d32359cb246ef34ed9
Eric Puentes <eric.david2333@gmail.com>
2021-09-21 13:55:40 -0500

    refactor completion, fix snippet expansion

--- nvim/.config/nvim/lua/ericus/completion.lua
+++ nvim/.config/nvim/lua/ericus/completion.lua
@@ -1,17 +1,37 @@
-local cmp = require "cmp"
-local lspkind = require "lspkind"
+-- Snippets
 local luasnip = require "luasnip"
 
-local t = require("ericus.vim-utils").t
-local cb = require("ericus.vim-utils").check_back_space
+luasnip.config.set_config {
+  -- Update more often, :h events for more info.
+  updateevents = "TextChanged,TextChangedI",
+  region_check_events = "CursorMoved",
+  delete_check_events = "TextChanged",
+}
+
+require("luasnip/loaders/from_vscode").lazy_load { include = { "lua", "go", "rust" } }
+
+-- cmp
+local cmp = require "cmp"
+
+local has_words_before = function()
+  if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
+    return false
+  end
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
+end
+
+local feedkey = function(key)
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), "n", true)
+end
 
 local function super_tab(fallback)
   if vim.fn.pumvisible() == 1 then
-    vim.fn.feedkeys(t "<C-n>", "n")
+    feedkey "<C-n>"
   elseif luasnip.expand_or_jumpable() then
-    vim.fn.feedkeys(t "<Plug>luasnip-expand-or-jump", "")
-  elseif cb() then
-    vim.fn.feedkeys(t "<tab>", "n")
+    luasnip.expand_or_jump()
+  elseif has_words_before() then
+    cmp.complete()
   else
     fallback()
   end
@@ -19,14 +39,15 @@ end
 
 local function super_s_tab(fallback)
   if vim.fn.pumvisible() == 1 then
-    vim.fn.feedkeys(t "<C-p>", "n")
+    feedkey "<C-p>"
   elseif luasnip.jumpable(-1) then
-    vim.fn.feedkeys(t "<Plug>luasnip-jump-prev", "")
+    luasnip.jump(-1)
   else
     fallback()
   end
 end
 
+local lspkind = require "lspkind"
 cmp.setup {
   snippet = {
     expand = function(args)
@@ -70,6 +91,3 @@ cmp.setup {
     { name = "path" },
   },
 }
-
--- Snippets
-require("luasnip/loaders/from_vscode").lazy_load { include = { "lua", "go", "rust" } }
