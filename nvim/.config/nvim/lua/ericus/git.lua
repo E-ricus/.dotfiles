940120e65c59a6727315c62a2aa795eba53f9109
Eric Puentes <eric.david2333@gmail.com>
2021-12-14 09:36:50 -0500

    git branche

--- nvim/.config/nvim/lua/ericus/git.lua
+++ nvim/.config/nvim/lua/ericus/git.lua
@@ -2,5 +2,5 @@
 local map = require("ericus.vim-utils").mapper
 
 map("n", "<leader>gg", "Neogit kind=split")
-map("n", "<leader>Gb", "Telescope git_branches")
+map("n", "<leader>gb", "Telescope git_branches")
 map("n", "<leader>gc", "Neogit commit")
