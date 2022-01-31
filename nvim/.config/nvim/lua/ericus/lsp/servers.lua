643aa09270a9818724837cdf8c1f53fb0035c8ae
Eric Puentes <eric.david2333@gmail.com>
2022-01-31 09:38:02 -0500

    null options

--- nvim/.config/nvim/lua/ericus/lsp/servers.lua
+++ nvim/.config/nvim/lua/ericus/lsp/servers.lua
@@ -73,9 +73,16 @@ M.setup_servers = function(on_attach, capabilities)
   null_ls.setup {
     sources = {
       null_ls.builtins.formatting.stylua,
-      null_ls.builtins.formatting.prettier,
-      null_ls.builtins.diagnostics.eslint,
-      null_ls.builtins.code_actions.eslint,
+      null_ls.builtins.formatting.prettierd.with {
+        prefer_local = "node_modules/.bin",
+      },
+      null_ls.builtins.diagnostics.eslint.with {
+        prefer_local = "node_modules/.bin",
+      },
+      null_ls.builtins.code_actions.eslint.with {
+        prefer_local = "node_modules/.bin",
+      },
+      null_ls.builtins.code_actions.gitsigns,
     },
     on_attach = on_attach,
   }
