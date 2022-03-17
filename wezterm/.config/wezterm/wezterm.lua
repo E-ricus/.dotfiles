b0441cd55fa391204bafca4ecb333b7809c46f95
Eric Puentes <eric.david2333@gmail.com>
2022-03-17 16:43:56 -0500

    wezterm keys

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -2,26 +2,35 @@ local wezterm = require("wezterm")
 
 local mykeys = {
 	{
-		key = "%",
-		mods = "CTRL|SHIFT",
+		key = "mapped:v",
+		mods = "LEADER",
 		action = wezterm.action({ SplitHorizontal = { domain = "CurrentPaneDomain" } }),
 	},
 	{
-		key = "s",
-		mods = "CTRL|SHIFT",
+		key = "mapped:s",
+		mods = "LEADER",
 		action = wezterm.action({ SplitVertical = { domain = "CurrentPaneDomain" } }),
 	},
+	{ key = "mapped:c", mods = "LEADER", action = wezterm.action({ SpawnTab = "CurrentPaneDomain" }) },
+	{ key = "mapped:V", mods = "CTRL", action = "Paste" },
+	{ key = "mapped:C", mods = "CTRL", action = "Copy" },
+	{ key = "mapped:h", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Left" }) },
+	{ key = "mapped:l", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Right" }) },
+	{ key = "mapped:k", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Up" }) },
+	{ key = "mapped:j", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Down" }) },
+	{ key = "mapped:q", mods = "CMD", action = "QuitApplication" },
 }
 for i = 1, 8 do
 	-- CTRL number to activate that tab
 	table.insert(mykeys, {
 		key = tostring(i),
-		mods = "CTRL",
+		mods = "LEADER",
 		action = wezterm.action({ ActivateTab = i - 1 }),
 	})
 end
 
 return {
+	leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 },
 	font = wezterm.font("JetBrainsMono Nerd Font"),
 	font_size = 17,
 	color_scheme = "DoomOne",
