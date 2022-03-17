8e68b854a236fee1cfb3ef05b5b63b04145c37c0
Eric Puentes <eric.david2333@gmail.com>
2022-03-17 13:50:50 -0500

    wezterm configs

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,6 +1,29 @@
 local wezterm = require("wezterm")
+
+local mykeys = {
+	{
+		key = "%",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action({ SplitHorizontal = { domain = "CurrentPaneDomain" } }),
+	},
+	{
+		key = "s",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action({ SplitVertical = { domain = "CurrentPaneDomain" } }),
+	},
+}
+for i = 1, 8 do
+	-- CTRL number to activate that tab
+	table.insert(mykeys, {
+		key = tostring(i),
+		mods = "CTRL",
+		action = wezterm.action({ ActivateTab = i - 1 }),
+	})
+end
+
 return {
-	font = wezterm.font("JetBrains Mono"),
-	font_size = 16,
+	font = wezterm.font("JetBrainsMono Nerd Font"),
+	font_size = 17,
 	color_scheme = "DoomOne",
+	keys = mykeys,
 }
