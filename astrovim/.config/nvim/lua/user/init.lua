c1a8da733a0d12b26a28b893a5f10bfa07b88a7a
Eric Puentes <eric.david2333@gmail.com>
2022-10-10 18:46:05 -0500

    astrovim keymaps

--- astrovim/.config/nvim/lua/user/init.lua
+++ astrovim/.config/nvim/lua/user/init.lua
@@ -4,8 +4,15 @@ local config = {
 	-- Set colorscheme to use
 	colorscheme = "default_theme",
 
-	-- Mapping data with "desc" stored directly by vim.keymap.set().
-	--
+	lsp = {
+		mappings = {
+			n = {
+				["gr"] = { "<cmd>Telescope lsp_references<cr>", desc = "Telescope go to reference" },
+				["gi"] = { "<cmd>Telescope lsp_implementations<cr>", desc = "Telescope go to implementations" },
+			},
+		},
+	},
+
 	-- Please use this mappings table to set keyboard mapping since this is the
 	-- lower level configuration and more robust one. (which-key will
 	-- automatically pick-up stored data by this setting.)
@@ -19,10 +26,18 @@ local config = {
 			["<leader><leader>"] = { "<c-^>", desc = "Previous Buffer" },
 			["<leader><right>"] = { "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer Tab" },
 			["<leader><left>"] = { "<cmd>BufferLineCyclePrev<cr>", desc = "Previous Buffer Tab" },
+			["gr"] = false,
+			["gi"] = false,
 		},
 		t = {
 			-- setting a mapping to false will disable it
 			-- ["<esc>"] = false,
+		},
+		i = {
+			["."] = { ".<c-g>u", desc = "Undo breakpoints" },
+			[","] = { ",<c-g>u", desc = "Undo breakpoints" },
+			["!"] = { "!<c-g>u", desc = "Undo breakpoints" },
+			["?"] = { "?<c-g>u", desc = "Undo breakpoints" },
 		},
 	},
 
@@ -32,10 +47,8 @@ local config = {
 			{ "machakann/vim-highlightedyank" },
 			{ "gruvbox-community/gruvbox" },
 			{ "catppuccin/nvim", as = "catppuccin" },
-
 		},
 	},
-
 }
 
 return config
