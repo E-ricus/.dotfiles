c876e72af219615ebfee58c535869d7f89395436
Eric Puentes <eric.david2333@gmail.com>
2023-07-26 11:09:34 +0200

    mason dependency order

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -5,19 +5,19 @@ local M = {
   event = "BufReadPre",
   dependencies = {
     {
-      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
       config = function()
-        require("mason").setup()
+        require("mason-lspconfig").setup {
+          ensure_installed = {
+            "rust_analyzer",
+            "lua_ls",
+          },
+        }
       end,
       dependencies = {
-        "williamboman/mason-lspconfig.nvim",
+        "williamboman/mason.nvim",
         config = function()
-          require("mason-lspconfig").setup {
-            ensure_installed = {
-              "rust_analyzer",
-              "lua_ls",
-            },
-          }
+          require("mason").setup()
         end,
       },
     },
