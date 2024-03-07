be8bd0d6ae833d3fc739f3df0131728de9ad4bd6
Eric Puentes <eric.david2333@gmail.com>
2024-03-07 20:09:44 +0100

    tmux and wezterm

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,16 +1,16 @@
 local wezterm = require("wezterm")
 
 local config = {
-	-- font = wezterm.font("JetBrainsMono Nerd Font"),
-	font_size = 16,
-	color_scheme = "Catppuccin Mocha",
-	hide_tab_bar_if_only_one_tab = true,
-	window_padding = {
-		left = 0,
-		right = 0,
-		top = 0,
-		bottom = 0,
-	},
+    -- font = wezterm.font("JetBrainsMono Nerd Font"),
+    font_size = 12,
+    color_scheme = "Catppuccin Mocha",
+    hide_tab_bar_if_only_one_tab = true,
+    window_padding = {
+        left = 0,
+        right = 0,
+        top = 0,
+        bottom = 0,
+    },
 }
 
 -- config.default_prog = { '/home/ericus/.cargo/bin/nu', '-l' }
