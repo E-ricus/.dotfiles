0d7ff1f072e39f8fbdb15783b96b0e516f8a3688
Eric Puentes <eric.david2333@gmail.com>
2024-04-24 21:43:46 +0200

    minify

--- nvim/.config/nvim/lua/plugins/plugins.lua
+++ nvim/.config/nvim/lua/plugins/plugins.lua
@@ -24,6 +24,7 @@ return {
     },
     {
         "ThePrimeagen/harpoon",
+        event = "VeryLazy",
         config = function()
             require("harpoon").setup {}
 
@@ -36,14 +37,28 @@ return {
         end,
     },
     {
-        "rmagatti/auto-session",
+        'echasnovski/mini.starter',
         lazy = false,
         config = function()
-            require("auto-session").setup {
-                log_level = "error",
-                auto_session_suppress_dirs = { "~/", "~/Documents", "~/Downloads", "/" },
-            }
+            require('mini.starter').setup()
         end,
+        version = false
+    },
+    {
+        "echasnovski/mini.sessions",
+        lazy = false,
+        config = function()
+            local mini = require('mini.sessions')
+            mini.setup {
+            }
+            local write = function()
+                local name = vim.fn.input("Session name: ")
+                mini.write(name)
+            end
+            vim.keymap.set("n", "<leader>sl", mini.read, { noremap = true, desc = "Load session" })
+            vim.keymap.set("n", "<leader>sw", write, { noremap = true, desc = "Write session" })
+        end,
+        version = false
     },
     {
         "echasnovski/mini.notify",
@@ -54,24 +69,79 @@ return {
         end,
     },
     {
-        "folke/which-key.nvim",
+        'echasnovski/mini.clue',
         event = "VeryLazy",
         config = function()
-            local wk = require "which-key"
-            wk.setup {}
-            wk.register {
-                mode = { "n", "v" },
-                ["g"] = { name = "+goto" },
-                ["]"] = { name = "+next" },
-                ["["] = { name = "+prev" },
-                ["<leader>d"] = { name = "+diagnostics" },
-                ["<leader>f"] = { name = "+find" },
-                ["<leader>l"] = { name = "+lsp" },
-                ["<leader>t"] = { name = "+toogle" },
-                ["<leader>g"] = { name = "+git" },
-                ["<leader>h"] = { name = "+harpoon" },
-            }
+            local miniclue = require('mini.clue')
+            miniclue.setup({
+                triggers = {
+                    -- Leader triggers
+                    { mode = 'n', keys = '<Leader>' },
+                    { mode = 'x', keys = '<Leader>' },
+
+                    -- Built-in completion
+                    { mode = 'i', keys = '<C-x>' },
+
+                    -- `g` key
+                    { mode = 'n', keys = 'g' },
+                    { mode = 'x', keys = 'g' },
+
+                    -- Marks
+                    { mode = 'n', keys = "'" },
+                    { mode = 'n', keys = '`' },
+                    { mode = 'x', keys = "'" },
+                    { mode = 'x', keys = '`' },
+
+                    -- Registers
+                    { mode = 'n', keys = '"' },
+                    { mode = 'x', keys = '"' },
+                    { mode = 'i', keys = '<C-r>' },
+                    { mode = 'c', keys = '<C-r>' },
+
+                    -- Window commands
+                    { mode = 'n', keys = '<C-w>' },
+
+                    -- `z` key
+                    { mode = 'n', keys = 'z' },
+                    { mode = 'x', keys = 'z' },
+
+                    -- `[]` keys
+                    { mode = 'n', keys = ']' },
+                    { mode = 'x', keys = ']' },
+                    { mode = 'n', keys = '[' },
+                    { mode = 'x', keys = '[' },
+                },
+                window = {
+                    delay = 350,
+                },
+
+                clues = {
+                    { mode = 'n', keys = '<Leader>s', desc = '+Session' },
+                    { mode = 'n', keys = '<Leader>f', desc = '+Finder' },
+                    { mode = 'n', keys = '<Leader>d', desc = '+Diagnostics' },
+                    { mode = 'n', keys = '<Leader>h', desc = '+Harpoon' },
+                    { mode = 'n', keys = '<Leader>l', desc = '+LSP' },
+                    { mode = 'n', keys = '<Leader>g', desc = '+Git' },
+                    { mode = 'n', keys = '<Leader>t', desc = '+Trouble' },
+                    miniclue.gen_clues.builtin_completion(),
+                    miniclue.gen_clues.g(),
+                    miniclue.gen_clues.marks(),
+                    miniclue.gen_clues.registers(),
+                    miniclue.gen_clues.windows(),
+                    miniclue.gen_clues.z(),
+                },
+            })
         end,
+        version = false
+    },
+
+    {
+        'echasnovski/mini.bracketed',
+        event = "VeryLazy",
+        config = function()
+            require('mini.bracketed').setup()
+        end,
+        version = false
     },
 
     -- Langs Enhacement
@@ -103,9 +173,9 @@ return {
                     end
 
                     -- Navigation
-                    map("n", "]c", function()
+                    map("n", "]g", function()
                         if vim.wo.diff then
-                            return "]c"
+                            return "]g"
                         end
                         vim.schedule(function()
                             gs.next_hunk()
@@ -113,9 +183,9 @@ return {
                         return "<Ignore>"
                     end, { expr = true, desc = "Git next change" })
 
-                    map("n", "[c", function()
+                    map("n", "[g", function()
                         if vim.wo.diff then
-                            return "[c"
+                            return "[g"
                         end
                         vim.schedule(function()
                             gs.prev_hunk()
