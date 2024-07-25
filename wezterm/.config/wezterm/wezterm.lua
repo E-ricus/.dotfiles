05bf8cb3b7cab9799bbfdb4fbb4c20df7dcfd238
Eric Puentes <eric.david2333@gmail.com>
2024-07-25 20:41:22 +0200

    wezterm

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require("wezterm")
 
 local config = {
     font = wezterm.font("JetBrainsMono Nerd Font"),
-    font_size = 19,
+    font_size = 16,
     color_scheme = "Catppuccin Mocha",
     hide_tab_bar_if_only_one_tab = true,
     window_padding = {
