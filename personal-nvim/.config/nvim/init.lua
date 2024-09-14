75373786b079b94723b6dbd10347001c13778d39
Eric Puentes <eric.david2333@gmail.com>
2024-09-14 13:51:05 +0200

    font size

--- personal-nvim/.config/nvim/init.lua
+++ personal-nvim/.config/nvim/init.lua
@@ -4,9 +4,9 @@ vim.g.maplocalleader = " "
 
 -- WTF NVIM
 -- I don't want these defaults?
-vim.keymap.del("n", "grr")
-vim.keymap.del("n", "gra")
-vim.keymap.del("n", "grn")
+-- vim.keymap.del("n", "grr")
+-- vim.keymap.del("n", "gra")
+-- vim.keymap.del("n", "grn")
 
 require "config.lazy"
 require "config.keymaps"
