2f804aee4de7cdc9b2acaf605420e044e8aeac28
Eric Puentes <eric.david2333@gmail.com>
2025-04-12 14:36:24 +0200

    lsp

--- lazy-nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ lazy-nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -3,8 +3,16 @@ return {
   opts = {
     inlay_hints = { enabled = false },
     servers = {
-      mojo = {}, -- Replace with the LSP you need
-      ols = {}, -- Replace with the LSP you need
+      mojo = {},
+      ols = {},
+      sourcekit = {
+        root_dir = function(filename, _)
+          local util = require("lspconfig.util")
+          return util.root_pattern("buildServer.json")(filename)
+            or util.root_pattern("*.xcodeproj", "*.xcworkspace")(filename)
+            or util.root_pattern("Package.swift")(filename)
+        end,
+      },
     },
   },
 }
