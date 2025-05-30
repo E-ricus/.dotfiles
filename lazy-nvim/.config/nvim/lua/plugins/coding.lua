a5cddea9f36594823afc4bab8bba6c89dcd224cc
Eric Puentes <eric.david2333@gmail.com>
2025-05-30 17:33:50 +0200

    markdown render

---
+++ lazy-nvim/.config/nvim/lua/plugins/coding.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "OXY2DEV/markview.nvim",
+    lazy = false,
+    opts = {
+      preview = {
+        filetypes = { "markdown", "codecompanion" },
+        ignore_buftypes = {},
+      },
+    },
+  },
+  -- TODO: Why is this not working
+  -- {
+  --   "MeanderingProgrammer/render-markdown.nvim", -- Make Markdown buffers look beautiful
+  --   event = "VeryLazy",
+  --   ft = { "markdown", "codecompanion" },
+  --   opts = {
+  --     render_modes = true, -- Render in ALL modes
+  --     sign = {
+  --       enabled = false, -- Turn off in the status column
+  --     },
+  --   },
+  -- },
+}
