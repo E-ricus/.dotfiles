84364f038023061b265ba2b38397d6e83bca9ee7
Eric Puentes <eric.david2333@gmail.com>
2025-06-09 17:31:25 +0200

    changes

--- lazy-nvim/.config/nvim/lua/plugins/coding.lua
+++ lazy-nvim/.config/nvim/lua/plugins/coding.lua
@@ -10,16 +10,4 @@ return {
     },
   },
   { "akinsho/toggleterm.nvim", version = "*", config = true },
-  -- TODO: Why is this not working
-  -- {
-  --   "MeanderingProgrammer/render-markdown.nvim", -- Make Markdown buffers look beautiful
-  --   event = "VeryLazy",
-  --   ft = { "markdown", "codecompanion" },
-  --   opts = {
-  --     render_modes = true, -- Render in ALL modes
-  --     sign = {
-  --       enabled = false, -- Turn off in the status column
-  --     },
-  --   },
-  -- },
 }
