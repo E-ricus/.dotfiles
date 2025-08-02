1dfbff2bd2547cc02dbe8bb94a6eae158cc904a5
Eric Puentes <eric.david2333@gmail.com>
2025-08-02 11:24:52 +0200

    asdf config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -50,7 +50,19 @@ add_to_path $HOME/linuxbrew/.linuxbrew/bin
 
 # Asdf
 if test -d $HOME/.asdf
-    source $HOME/.asdf/asdf.fish
+    # ASDF configuration code
+    if test -z $ASDF_DATA_DIR
+        set _asdf_shims "$HOME/.asdf/shims"
+    else
+        set _asdf_shims "$ASDF_DATA_DIR/shims"
+    end
+
+    # Do not use fish_add_path (added in Fish 3.2) because it
+    # potentially changes the order of items in PATH
+    if not contains $_asdf_shims $PATH
+        set -gx --prepend PATH $_asdf_shims
+    end
+    set --erase _asdf_shims
 end
 
 # Bun
